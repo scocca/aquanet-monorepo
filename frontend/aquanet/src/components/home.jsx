ad50aa526a43914068d2ea323ef08be164b4a9c5
scocca <s.cocca.u@gmail.com>
2023-11-02 17:21:09 -0300

    se eliminan zone identifier y se cambia el estilo del logo aquanet el home

--- frontend/aquanet/src/components/home.jsx
+++ frontend/aquanet/src/components/home.jsx
@@ -2,13 +2,14 @@ const Home =()=>{
     return(
         <div className="container">
             <div className="description">
-                <h1 className="title">AquaNet</h1>
+                <h1 className="title">aqua</h1>
+                <h1 className="title-2">net</h1>
                 <p className="slogan">Agua Limpia comunidades fuertes</p>
                 <p className="description-one">En Chile la escasez de agua es un desafío importante, especialmente en el norte y centro. Esto puede generar competencia entre la agricultura y el saneamiento humano por los recursos hídricos.</p>
             </div>
         <section  className="login-container">
             <div className="login">
-                <h2 className="name">Aqua</h2>
+                <h2 className="name">aqua</h2>
                 <h2 className="name-2">net</h2>
                 <div className="login-user">
                 <p className='enter'>Ingresa</p>
