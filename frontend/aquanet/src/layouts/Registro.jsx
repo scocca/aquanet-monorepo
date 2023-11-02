0432f9de9306649a482f92ac95728e8fce1562c2
Ignacio Toro <ignacio.thoro@gmail.com>
2023-11-02 19:35:41 -0400

    se agregó código a Registro

---
+++ frontend/aquanet/src/layouts/Registro.jsx
@@ -0,0 +1,17 @@
+
+import './Login.css';
+
+import { Home } from '../components/home'
+import { Footer } from '../components/footer';
+
+
+const Registro =() =>{
+    return(
+    <div className="Home">
+      <Home/>
+      <Footer/>
+    </div>
+    )
+  }
+
+  export {Registro}
