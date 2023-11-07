ed6571f4f8ae49712de77c6f9d158e7c674381a0
Ssim77 <Castillocisternas1994@gmail.com>
2023-11-06 22:17:35 -0300

    Cambio final

--- frontend/aquanet/src/components/TxtConfReg.jsx
+++ frontend/aquanet/src/components/TxtConfReg.jsx
@@ -1,16 +1,16 @@
-const TxtConfReg =()=>{
+const TxtConfReg =({logoapro})=>{
     return(
-        <div className="container">
+        <div className="container-reg">
             <div>
             </div>
-        <section  className="login-container">
-            <div className="login">
-                <div className="login-user">
+        <section  className="login-container-reg">
+            <div className="login-reg">
+                <div className="login-user-reg">
                 <p className='enter'>Felicidades!, tu cuenta  ha sido creada con éxito. Te hemos enviado un mail de confirmación al correo :</p>              
                 <p className='corr2'>Usuario574@AquaNet.cl</p> 
-                <img src="./assets/Icons/🦆 icon _check circle_.png"/> 
+                <img src = {logoapro}/>
                 </div>
-                <button className="log-in-btn">Volver</button>
+                <button className="reg-in-btn2-reg">Volver </button>
             </div>
             </section>
         </div>
--- frontend/aquanet/src/layouts/ConfirmacionRegistro.jsx
+++ frontend/aquanet/src/layouts/ConfirmacionRegistro.jsx
@@ -3,15 +3,26 @@ import './ConfirmacionRegistro.css'
 import { NavBar } from '../components/NavBar'
 import { TxtConfReg } from '../components/TxtConfReg'
 import { Footer } from '../components/footer';
-import Regicon from '../assets/Icons/🦆 icon _check circle_.png'
+import regicon from '../assets/Icons/icon _check circle_.png'
 
 
 const ConfirmacionRegistro =() =>{
     return(
     <div className="TxtConfReg">
-      <NavBar/>
-      <TxtConfReg/>
-      <Footer/>
+      <section className='reg-section'>
+      <h1 className='title-reg-main'>Registro</h1>
+      <TxtConfReg logoapro={regicon}/>
+      </section>
+      <section className='watermark-reg'>
+        <div className='watermark-reg-container'>
+          <p className='watermark-p'>
+            2023
+          </p>
+          <p className='watermark-link'>
+             AquaNet.cl
+          </p>
+        </div>
+      </section>
     </div>
     )
   }
