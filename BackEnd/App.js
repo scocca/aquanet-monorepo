aa1359fdefcf40576383a70b0078e318191fa401
maratoro <maratoro1155@gmail.com>
2023-11-17 11:34:59 -0300

    uu

---
+++ BackEnd/App.js
@@ -0,0 +1,21 @@
+/*const Configuracion= require("openai").Configuracion;
+const OpenAiApi= require("openai").OpenAiApi;
+
+const Configuracion= new Configuracion({
+apiKey= "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";
+organizacion= "org-xblPJfYA8BL3xZcckxWxwATO";
+
+});
+const openai = new OpenAiApi(Configuracion);
+
+async function App(){
+const response= await openai.createCompletion({ 
+    model = "text-davinci-003",
+prompt
+    })
+}*/
+
+    
+
+
+ 
---
+++ frontend/aquanet/src/components/Traduccion.jsx
@@ -0,0 +1,31 @@
+/*import React, { useState } from 'react';
+import { GoogleTranslateElement } from 'react-google-translate-component';
+
+const Traduccion = () => {
+  const [isGoogleTranslateLoaded, setIsGoogleTranslateLoaded] = useState(false);
+
+  const handleGoogleTranslateLoad = () => {
+    setIsGoogleTranslateLoaded(true);
+  };
+
+  return (
+    
+    <div id="google_translate_element">
+    <div>
+      {isGoogleTranslateLoaded && (
+        <GoogleTranslateElement
+          pageLanguage={'en'} 
+        />
+       
+      )}
+</div>
+      <script
+        type="text/javascript"
+        src="https://translate.google.com/translate_a/element.js?cb=handleGoogleTranslateLoad"
+        async={true}
+      />
+    </div>
+  );
+};
+
+export default Traduccion;*/
