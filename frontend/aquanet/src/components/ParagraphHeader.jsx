c27cf3a878aabd05061b04d55d7621e83727a749
arisevilla <arisevilla93@gmail.com>
2023-11-20 16:27:48 -0300

    estados de renderizacion regiones

--- frontend/aquanet/src/components/ParagraphHeader.jsx
+++ frontend/aquanet/src/components/ParagraphHeader.jsx
@@ -1,32 +1,21 @@
-import React, { useState } from 'react'
+import React from 'react'
 
-const ParagraphHeader = () => {
-  const [regionValor, setregionValor] = useState("");
+const ParagraphHeader = (props) => {
+  
 
-  const handleRegion = (e) => {
-    console.log(e.target.value);
-    setregionValor(e.target.value)
-  }
+  const{name,texto} = props;
 
 
   return (
     <div className="info-container">
       <div className="name-region">
-      <h1 className="name-region-p">Región de los Lagos</h1>
-            <form className='formBlog'>
-              <label for="opcionesRegiones" className='titleformBlog'></label>
-              <select className='opcionesRegiones' name="regiones" onChange={handleRegion}>
-                <option className='opcionesBlog' value="Araucania">Araucania</option>
-                <option className='opcionesBlog' value="Coquimbo">Coquimbo</option>
-                <option className='opcionesBlog' value="Valparaiso">Valparaíso</option>
-              </select>
-            </form>
+      <h1 className="name-region-p">{name}</h1>
             
-            
+           
         </div>
         <div>
             <div className="info-region-container">
-                <p className="info-region">Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim blanditiis odio, ipsum voluptates non temporibus! Ipsum provident maxime, libero totam corrupti adipisci aspernatur! Impedit ad veritatis praesentium, deleniti recusandae voluptatum?</p>
+                <p className="info-region">{texto}</p>
             </div>
         </div>
 
