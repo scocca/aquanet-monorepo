f8c53b3d254cd9526491f0e66e220b19e982836d
Arisevilla <arisevilla93@gmail.com>
2023-11-23 01:13:44 -0300

    se conecta la api por id

--- Aquanet-monorepo/frontend/src/layouts/Registro.jsx
+++ Aquanet-monorepo/frontend/src/layouts/Registro.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import '../layouts/Registro.css'
 import { useState } from "react"
 import { Link } from "react-router-dom"
-import axios from 'axios'
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
 const Registro =() =>{
 
